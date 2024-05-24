2880747a71548f28fab8c4f0fa71c0eb42014a74
pratsj22 <prathmeshjain42@gmail.com>
2024-05-25 00:10:08 +0530

    deployment configurations

--- backend/netlify/functions/server.js
+++ backend/netlify/functions/server.js
@@ -8,21 +8,21 @@ import orderRoute from '../../routes/orderRoute.js'
 import cors from 'cors';
 import serverless from "serverless-http";
 
-dotenv.config();
-connectDB();
-const app= express();
+dotenv.config()
+connectDB()
+const app= express()
 const port=  process.env.PORT || 8080;
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 app.get('/',(req,res)=>{
     res.send("hello")
 })
 //routes
-app.use('/.netlify/functions/server/api/v1/auth', authRoutes);
-app.use('/.netlify/functions/server/api/v1/category', categoryRoute);
-app.use('/.netlify/functions/server/api/v1/products', productRoute);
-// app.use('/.netlify/functions/server/api/v1/orders', orderRoute);
+app.use('/api/v1/auth',authRoutes)
+app.use('/api/v1/category',categoryRoute)
+app.use('/api/v1/products',productRoute)
+// app.use('/api/v1/orders',orderRoute)
 
 app.listen(port,()=>{
     console.log("Server running on port",port);
