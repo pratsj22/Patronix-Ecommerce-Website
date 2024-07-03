76e00ae5c286d40ae3034ee3b9ac36636dd9a7b4
pratsj22 <prathmeshjain42@gmail.com>
2024-07-03 20:52:21 +0530

    deployment configurations

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Slider from '../../components/Slider/Slider'
 import FeaturedProd from '../../components/FeaturedProd/FeaturedProd'
 import Categories from '../../components/Categories/Categories'
 import Contact from '../../components/Contact/Contact'
-
+import axios from 'axios'
 const Home = () => {
+  useEffect(()=>{
+    const intervalId=setInterval(()=>{
+      axios.get(`${process.env.REACT_APP_API_URL}`)
+    },1200000)
+    return ()=> clearInterval(intervalId);
+  },[])
   return (
     <div>
       <Slider />
