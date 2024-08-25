f3d3ee2a8542e6bddf53a4eec3849cd7d3160043
pratsj22 <prathmeshjain42@gmail.com>
2024-08-26 01:11:01 +0530

    design changes

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -119,7 +119,7 @@ const Cart = ({ open, setOpen }) => {
                                     <div className="mt-6">
                                         <div
                                             onClick={handleCheckOut}
-                                            className="flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-blue-700"
+                                            className="flex items-center justify-center rounded-md border cursor-pointer border-transparent bg-blue-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-blue-700"
                                         >
                                             Checkout
                                         </div>
